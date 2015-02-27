093671b21357b710e81318338c09d1458ec27561
xiaojun <mengxj.nus@gmail.com>
2015-02-27 20:46:01 +0800

    Test for AnjularJS binding to localstorage

--- js/initData.js
+++ js/initData.js
@@ -4,12 +4,15 @@ app.config(function(localStorageServiceProvider){
   localStorageServiceProvider
     .setPrefix('nexp')
     .setStorageType('localStorage')
+    //.setStorageType('sessionStorage')
     .setStorageCookieDomain('example.com')
     .setNotify(true, true);
 });
 
 app.controller('initData', function($scope, $http, localStorageService){
 	localStorageService.clearAll();
+
+	localStorageService.set('localStorageDemo', 'oldValue');
 
 	if (localStorageService.getStorageType().indexOf('session') >= 0) {
       console.log('StorageType: Session storage');
@@ -25,8 +28,3 @@ app.controller('initData', function($scope, $http, localStorageService){
 	});
 
 });
-
-function createJsonData(){
-	var jsonData = {"design_guide":{"research_question":{"general_question":"What is the interactive effect of density & the number of dimension for PCP & SCP-standard?","experiment_title":"PCP vs. SCP","experiment_description":"Xiaojun","experiment_conductor":"There seems to be a Density & Number of Dimension Trade-off between PCP & SCP-common. I try to figure out the specific interactive effect here.","hypothesis":{"main_solution":"SCP","compare_solutions":{"compare_solution":"PCP"},"tasks":{"task":"Density"},"contexts":{"context":"Number of dimension"},"measures":{"measure":["Time cost","Accuracy"]},"target_population":""}},"variables":{"independent_variable":[{"name":"Technique","subject_design":"Within","levels":{"level":[{"name":"SCP"},{"name":"PCP"}]},"counter_balance":"FullyCounterBalancing"},{"name":"Density","subject_design":"Within","levels":{"level":[{"name":"4D"},{"name":"6D"}]},"counter_balance":"FullyCounterBalancing"},{"name":"Number of dimension","subject_design":"Within","levels":{"level":[{"name":"20"},{"name":"40"}]},"counter_balance":"FullyCounterBalancing"}],"dependent_variable":[{"name":"Time cost"},{"name":"Accuracy"}]},"arrangement":{"min_number":"8","actual_number":"16","fee_per_participant":"10","trial":"3","time_per_trial":"200","block":"2","participants":""}}};
-	return JSON.parse(jsonData);
-}
--- js/pageOneControl.js
+++ js/pageOneControl.js
@@ -1,6 +1,23 @@
-var app = angular.module("nexp", []);
+var app = angular.module("nexp", ['LocalStorageModule']);
 
-app.controller('pageOneControl', ['$scope','$rootScope',
-  function($scope, $rootScope) {
-    console.log($rootScope);
-}]);
+app.config(function(localStorageServiceProvider){
+  localStorageServiceProvider
+    .setPrefix('nexp')
+    .setStorageType('localStorage')
+    //.setStorageType('sessionStorage')
+    .setStorageCookieDomain('example.com')
+    .setNotify(true, true);
+});
+
+app.controller('pageOneControl', function($scope, $http, localStorageService){
+	console.log(localStorageService.get('localStorageDemo'));
+
+	if (localStorageService.getStorageType().indexOf('session') >= 0) {
+      console.log('StorageType: Session storage');
+    }
+
+    if (!localStorageService.isSupported) {
+      console.log('StorageType: Cookie');
+    }
+
+});
